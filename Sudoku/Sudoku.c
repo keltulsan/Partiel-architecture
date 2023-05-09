72d66f3861b8181407c43936af0256c3426262b2
ebeaufils <ebeaufils@gaming.tech>
2023-05-09 15:58:56 +0200

    feature : createGrid

--- Sudoku/Sudoku.c
+++ Sudoku/Sudoku.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void createGrid();
+
+
 
 typedef struct Case
 {
@@ -12,9 +15,14 @@ int main()
 {
 	int nbLigne = 9;
 	int nbColonne = 9;
+	createGrid(nbLigne, nbColonne);
 }
 
 void createGrid(int nbLigne, int nbColonne)
 {
-
-}
+	Case** Sudoku = (Case**)malloc(sizeof(Case*) * nbLigne);
+	for (int i = 0; i < nbColonne; i++)
+	{
+		Sudoku[i] = (Case*)malloc(sizeof(Case) * nbColonne);
+	}
+} 
