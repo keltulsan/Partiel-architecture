4d5a949b02b5148bf1d5c637ab080706dea2fc07
ebeaufils <ebeaufils@GCFRLY238>
2023-05-09 15:49:52 +0200

    Ajoutez des fichiers projet.

---
+++ Sudoku/Sudoku.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct Case
+{
+	int number;
+}Case;
+
+
+int main()
+{
+	int nbLigne = 9;
+	int nbColonne = 9;
+}
+
+void createGrid(int nbLigne, int nbColonne)
+{
+
+}
