df67bb4909f34b50f20a74751f8d3112ae6ccc94
ebeaufils <ebeaufils@gaming.tech>
2023-05-09 16:44:27 +0200

    feature : askPosition/askNumber

--- Sudoku/Sudoku.c
+++ Sudoku/Sudoku.c
@@ -12,6 +12,8 @@ typedef struct Case
 Case** createGrid();
 void initGrid();
 void displayGrid();
+void askPosition();
+char askNumber();
 
 
 
@@ -19,10 +21,16 @@ int main()
 {
 	int nbLigne = 9;
 	int nbColonne = 9;
+	int victory = 0;
 	Case** grid = createGrid(nbLigne, nbColonne);
 
 	initGrid(grid, nbLigne, nbColonne);
 	displayGrid(grid, nbLigne, nbColonne);
+	while (victory == 0)
+	{
+		askPosition(grid, nbLigne, nbColonne);
+		displayGrid(grid, nbLigne, nbColonne);
+	}
 }
 
 Case** createGrid(int nbLigne, int nbColonne)
@@ -37,7 +45,7 @@ Case** createGrid(int nbLigne, int nbColonne)
 
 void initGrid(Case** grid, int nbLigne, int nbColonne)
 {
-	Case** odefault = {'0'};
+	Case** odefault = {'X'};
 	for (int i = 0; i < nbLigne; i++)
 	{
 		for (int j = 0; j < nbColonne; j++)
@@ -59,3 +67,61 @@ void displayGrid(Case** grid, int nbLigne, int nbColonne)
 	}
 	printf("\n");
 }
+
+void askPosition(Case** grid, int nbLigne, int nbColonne)
+{
+	int i = 0;
+	int j = 0;
+	printf("Ou voulez vous placer votre prochian nombre ?  ligne/colonne : \n");
+	scanf_s("%d/%d", i, j);
+	if (i < 1 || i > nbLigne)
+	{
+		printf("La ligne selectionnee est incorecte : \n");
+		scanf_s("%d/", i);
+	}
+	else if (j < 1 || j >nbColonne)
+	{
+		printf("La colonne selectionnee est incorecte : \n");
+		scanf_s("%d/", j);
+	}
+
+	grid[i-1][j-1].number = askNumber(grid, nbLigne, nbColonne);
+
+}
+
+char askNumber(Case** grid, int nbLigne, int nbColonne)
+{
+	int number = 1;
+	printf("Quel nombre voulez vous placer ? entre 1 et 9 : \n");
+	scanf_s("%c", number);
+	if (number != '1' && number != '2'&& number != '3'&& number != '4'&& number != '5'&& number != '6'&& number != '7'&& number != '8'&& number != '9')
+	{
+		printf("La nombre selectionne est incorecte : \n");
+		scanf_s("%c/", number);
+	}
+	return number;
+}
+
+/*int verifNumbers(Case** grid, int nbLigne, int nbColonne, int* victory)
+{
+	char tab = malloc(sizeof(char) * 9);
+	int notIn = 1;
+	for (int i = 0; i < nbLigne; i++)
+	{
+		for (int j = 0; j < nbColonne; j++)
+		{
+			for (int el = 0; el < 9; el++)
+			{
+				if (tab[el] == grid[i][j].number)
+				{
+					notIn = 0;
+				}
+			}
+			if (notIn == 1)
+			{
+
+			}
+		}
+	*/
+
+
